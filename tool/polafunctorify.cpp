ec5e80c19fc8ab5e3be1c44673e167f91ad530fe
rob <rob@mediabox.polacanthus.net>
2010-10-25 23:17:39 +0200

    Added a start of a CMakeLists.txt with the base dependency checks for a few boost header files we will likely be using.

---
+++ tool/polafunctorify.cpp
@@ -0,0 +1,8 @@
+#include <boost/program_options.hpp>
+#include "GccXmlAbstractClass.hpp"
+#include "GccXmlAbstractClassFinder.hpp"
+#include "FunctorifyGenerator.hpp"
+
+int main(int argc, char *argv[]) {
+  
+}
